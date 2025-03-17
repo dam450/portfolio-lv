efa517ab07f701a3ecef42415bea373d76809b41
Evandro <dam450@duck.com>
2025-03-17 14:21:19 -0300

    chore: update contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,15 @@
 export const Contact = () => {
   return (
     <section id="contact" className="py-32  text-center">
-      <p className="mb-16 lg:text-lg">Vamos conversar?</p>
-      <h2 className="font-bold text-5xl lg:text-6xl mb-3">
+      <p className="mb-16 lg:text-lg content-show">Vamos conversar?</p>
+      <h2 className="font-bold text-5xl lg:text-6xl mb-3 content-show">
         Contrate-me e descubra
       </h2>
-      <p className="pb-16 text-lg">
+      <p className="pb-16 text-lg content-show">
         Como vamos não apenas programar, mas criar um projeto incrível.
       </p>
       <a
-        className="border rounded-full px-8 py-4 font-bold hover:bg-secondary-50 hover:text-secondary-900 transition-colors duration-300"
+        className="border rounded-full px-8 py-4 font-bold hover:bg-secondary-50 hover:text-secondary-900 transition-colors duration-300 content-show"
         href="mailto:"
       >
         Entre em contato
