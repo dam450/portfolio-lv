6af3f48bab7923b9faf53e3d6de1f4093dfb933f
Evandro <dam450@duck.com>
2025-03-17 11:59:56 -0300

    feat: create contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,27 +1,19 @@
 export const Contact = () => {
   return (
-    <>
-      <style>
-        {`
-        #contact {
-          height: 100vh;
-          background-color: #dfd;
-          padding-top: 5rem;
-          text-align: center;
-          color: #333;
-        }
-        #contact h2 {
-          margin-bottom: 10px;
-        }
-        #contact p {
-          margin-bottom: 10px;
-        }
-      `}
-      </style>
-      <section id="contact">
-        <h2>Contact Us</h2>
-        <p>Send us a message!</p>
-      </section>
-    </>
+    <section id="contact" className="py-32  text-center">
+      <p className="mb-16 lg:text-lg">Vamos conversar?</p>
+      <h2 className="font-bold text-5xl lg:text-6xl mb-3">
+        Contrate-me e descubra
+      </h2>
+      <p className="pb-16 text-lg">
+        Como vamos não apenas programar, mas criar um projeto incrível.
+      </p>
+      <a
+        className="border rounded-full px-8 py-4 font-bold hover:bg-secondary-50 hover:text-secondary-900 transition-colors duration-300"
+        href="mailto:"
+      >
+        Entre em contato
+      </a>
+    </section>
   );
 };
