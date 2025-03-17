b4e91752d9231f6c2acf95bec15cf41b109623c4
Evandro <dam450@duck.com>
2025-03-17 12:01:57 -0300

    chore: attach page sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Hero } from "./components/Hero";
+import { Offer } from "./components/Offer";
+import { Projects } from "./components/Projects";
+import { Stacks } from "./components/Stacks";
 import { Layout } from "./layouts/BaseLayout";
 
-function App() {
+export function App() {
   return (
     <>
       <Layout>
         <Hero />
+        <Offer />
+        <Stacks />
         <About />
+        <Projects />
         <Contact />
       </Layout>
     </>
